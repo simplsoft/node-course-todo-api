a45f1f493900f16a2a27e854a20c8b7935e01d8e
Valentin Bogdan <pvj.sourcing@gmail.com>
2018-09-06 13:39:12 +0200

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,29 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  } 
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5b8fbbffff106419149a7926')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({
+    name: 'Valentin'
+  }).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch Users', err);
+  });
+
+  // client.close();
+});
