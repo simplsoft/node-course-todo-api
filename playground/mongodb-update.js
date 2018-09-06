e1c5b4341bc1d50b21cb91ca43de28d19c07b59b
Valentin Bogdan <pvj.sourcing@gmail.com>
2018-09-06 17:27:16 +0200

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  } 
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5b912d03c1fe0b886e3f9a31')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b9005f68cb06b7dba247ad2')
+  }, {
+    $set: {
+      name: 'Valentin',
+      location: 'Hamburg',
+      country: 'Germany'
+    },
+    $inc: {
+      age: -1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // client.close();
+});
